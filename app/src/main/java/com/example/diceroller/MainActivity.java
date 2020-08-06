b022495fda7ab837924f1a1adc81d25a0e8215ac
Param Siddharth <paramsiddhartha@gmail.com>
2020-08-06 16:31:40 +0530

    Implement visuals

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.diceroller;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,20 +35,22 @@ public class MainActivity extends AppCompatActivity {
         final Dice myDie = new Dice(6);
         final int dieRoll = myDie.roll();
 
-        final TextView resultTextView = findViewById(R.id.textView2);
+        // final TextView resultTextView = findViewById(R.id.textView2);
+        final ImageView dieImage = findViewById(R.id.imageView);
 
         int drawableResource = 0;
         switch (dieRoll) {
-            case 1: drawableResource = 1; break;
-            case 2: drawableResource = 2; break;
-            case 3: drawableResource = 3; break;
-            case 4: drawableResource = 4; break;
-            case 5: drawableResource = 5; break;
-            default: drawableResource = 6;
+            case 1: drawableResource = R.drawable.dice_1; break;
+            case 2: drawableResource = R.drawable.dice_2; break;
+            case 3: drawableResource = R.drawable.dice_3; break;
+            case 4: drawableResource = R.drawable.dice_4; break;
+            case 5: drawableResource = R.drawable.dice_5; break;
+            default: drawableResource = R.drawable.dice_6;
         }
 
         // Display the result
-        resultTextView.setText(Integer.toString(drawableResource));
+        // resultTextView.setText(Integer.toString(drawableResource));
+        dieImage.setImageResource(drawableResource);
 
         // Show toast if asked for
         if (showToast) {
